3d2de3c0b3d11b236eac46bf63d205d842d98846
atulbansalofficial <atulbansal9@gmail.com>
2022-01-03 20:05:13 +0530

    event in react

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,73 +1,54 @@
 import React, { useState } from "react";
 
 function Login() {
-  const [userId, setUserId] = useState("");
-  const [password, setPassword] = useState("");
-  const [userError, setUserError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [getData, setData] = useState([]);
+  const [data, setData] = useState("");
+  const [pass, setPass] = useState("");
+  const [value, setValue] = useState(false);
+  // let data = "atul bansal";
+  // let pass = "8871075742";
 
   const loginHandler = (e) => {
-    if (userId.length < 3 || password.length < 3) {
-      alert("type Correct Value");
-    } else {
-      alert("All Good!");
-    }
-    e.preventDefault();
-    console.log("hi");
-    let value = [...getData];
-
-    setData([...value, userId, password]);
-    console.log(getData);
+    // data = "Iron Man";
+    // pass = "0000000007";
+    // alert(data);
+    // alert(pass);
+    console.log(data, pass);
+    setValue(true)
   };
-  const userHandler = (e) => {
-    if (e.target.value.length < 3) {
-      setUserError(true);
-    } else {
-      setUserError(false);
-    }
-
-    setUserId(e.target.value);
+  const getuserId = (e) => {
+    console.log(e.target.value);
+    setData(e.target.value);
   };
-  const passwordHandler = (e) => {
-    if (e.target.value.length < 3) {
-      setPasswordError(true);
-    } else {
-      setPasswordError(false);
-    }
-
-    setPassword(e.target.value);
+  const getuserPass = (e) => {
+    console.log(e.target.value);
+    setPass(e.target.value);
   };
+
   return (
     <div>
       <h1>Login Page</h1>
-      <form onSubmit={loginHandler}>
+      <div>
         <input
           type="text"
+          value={data}
           placeholder="Enter User id"
-          value={userId}
-          onChange={(data) => {
-            userHandler(data);
-          }}
+          onChange={getuserId}
         />
-        {userError ? <span>User Not Valid</span> : ""}
-        <br />
-        <br />
         <input
+          value={pass}
           type="text"
           placeholder="Enter User password"
-          onChange={(data) => {
-            passwordHandler(data);
-          }}
-        />{" "}
-        {passwordError ? <span>Password Not Valid</span> : ""}
-        <br />
-        <br />
-        <button type="submit">Login</button>
-      </form>
+          onChange={getuserPass}
+        />
 
-      <p> User Id :-{userId}</p>
-      <p> Password :-{password}</p>
+        <button onClick={loginHandler}>Login</button>
+      </div>
+      {value ? (
+        <>
+          <h1>ID :{data}-</h1>
+          <h1>Pass:-{pass}</h1>
+        </>
+      ) : null}
     </div>
   );
 }
