6182fcac8eae88e31fbe393fdc6e2348816f0179
atulbansalofficial <atulbansal9@gmail.com>
2022-01-04 12:33:03 +0530

    handle form with checkbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from "react";
-import Login from "./component/Login";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [selecter, setSelector] = useState("");
+  const [tnc, setTnc] = useState(false);
+ 
+
+  const getFormData = (e) => {
+    e.preventDefault();
+  
+   console.log(name,selecter,tnc);
+   
+    
+   
+    
+  };
+  
   return (
     <div>
       <div className="container ">
-        <h1>Basic Form Validation</h1>
-        <Login />
+        <h1>Handle Form in React</h1>
+        <form onSubmit={getFormData}>
+          <input
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e)=> setName(e.target.value)}
+          />
+          <br />
+          <br />
+          <select onChange={(e)=> setSelector(e.target.value)}>
+            <option value="0">Select Option</option>
+            <option value="1">Marvel</option>
+            <option value="2">DC</option>
+            <option value="3">starMan</option>
+          </select>
+          <br />
+          <br />
+          <input type="checkbox" onChange={(e) => setTnc(e.target.checked)} />
+          &nbsp;&nbsp;
+          <span>Accept tearm & Condition</span>
+          <br />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
