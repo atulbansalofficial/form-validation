58ab19127e7ab3ce9d9f58bf91d9375f65b24856
atulbansalofficial <atulbansal9@gmail.com>
2022-01-09 17:20:00 +0530

    use form hook

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import Form from "./component/Form";
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    trigger,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
-  };
   return (
-    <div className="conatiner">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="col-md-6  ">
-          <label htmlFor="firstname " className="form-label">
-            First Name<span style={{ color: "red" }}>*</span>
-          </label>
-          <input
-            id={"firstname"}
-            type="text"
-            placeholder="First Name"
-            className={`form-control  ${errors.firstname && true}`}
-            {...register("firstname", {
-              required: "First name is required",
-              minLength: {
-                value: 2,
-                message: "Please enter a valid name",
-              },
-
-              maxLength: {
-                value: 50,
-                message: "maximum length allowed is 50 word",
-              },
-              pattern: {
-                value: /^[A-Za-z]+$/i,
-                message: " Please enter fully alphabate",
-              },
-            })}
-            onKeyUp={() => {
-              trigger("firstname");
-            }}
-          />
-
-          {errors.firstname && (
-            <span style={{ color: "red" }}>{errors.firstname.message}</span>
-          )}
-        </div>
-      </form>
+    <div>
+      <Form />
     </div>
   );
 }
