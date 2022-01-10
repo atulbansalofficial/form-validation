5d942decb52a690ed3daa25212fbc4737af24979
atulbansalofficial <atulbansal9@gmail.com>
2022-01-10 15:07:40 +0530

    update email field

--- src/PostForm.js
+++ src/PostForm.js
@@ -2,22 +2,24 @@ import React, { useState } from "react";
 import Axios from "axios";
 
 function PostForm() {
-  const url = "http://localhost:4000/axios";
+  const url = "http://localhost:4000/api/students";
   const [data, setData] = useState({
-    fname: "",
-    lname: "",
+    first_name: "atul",
+    last_name: "bansal",
+    email: "atulbansal@gmail.com",
   });
 
   function submit(e) {
     e.preventDefault();
-    Axios.post(url, {
-      firstname: data.fname,
-      lastname: data.lname,
-    }).then((res) => {
-      console.log(res.data);
-    });
-  }
 
+    // Axios.post(url, {
+    //   first_name: data.first_name,
+    //   last_name: data.last_name,
+    //   email: data.email,
+    // }).then((res) => {
+    //   console.log(res.data);
+    // });
+  }
   function handle(e) {
     // console.log(e.target.value);
     const newData = { ...data };
@@ -25,29 +27,38 @@ function PostForm() {
     setData(newData);
     console.log(newData);
   }
-
   return (
     <div>
       <div className="container mt-5 d-flex justify-content-center bg-dark  p-5 text-white">
         <form onSubmit={(e) => submit(e)}>
-          <label htmlFor="fname">First Name : &nbsp;</label>
+          <label htmlFor="first_name">First Name : &nbsp;</label>
           <input
             onChange={(e) => handle(e)}
             type="text"
-            id="fname"
+            id="first_name"
             placeholder="First Name"
           />
           <br />
           <br />
-          <label htmlFor="lname">Last Name : &nbsp;</label>
+          <label htmlFor="last_name">Last Name : &nbsp;</label>
           <input
             onChange={(e) => handle(e)}
             type="text"
-            id="lname"
+            id="last_name"
             placeholder="Last Name"
           />
           <br />
           <br />
+          <label htmlFor="email">Email : &nbsp;</label>
+          <input
+            onChange={(e) => handle(e)}
+            type="email"
+            id="email"
+            placeholder="E-mail"
+          />
+          <br />
+          <br />
+
           <button>Submit</button>
         </form>
       </div>
